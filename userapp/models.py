b587b9a91573969a7588d17ee807c43b85c13e3c
Md. Shihab Uddin <pythonpiash@gmail.com>
2023-11-23 22:20:44 +0600

    add model and serializer for userapp

---
+++ userapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Parent(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+
+
+class Child(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE, related_name='children')
